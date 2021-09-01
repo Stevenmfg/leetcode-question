b8c69eb3984f8dd548369ad232dfd271000e47e2
steven <mfg5271314@163.com>
2021-09-01 21:07:23 +0800

    146:LRU缓存机制

---
+++ src/main/java/com/mengfg/leetcode/editor/cn/Problem_146_LruCache.java
@@ -0,0 +1,182 @@
+//运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制 。 
+//
+// 
+// 
+// 实现 LRUCache 类： 
+//
+// 
+// LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存 
+// int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。 
+// void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上
+//限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。 
+// 
+//
+// 
+// 
+// 
+//
+// 进阶：你是否可以在 O(1) 时间复杂度内完成这两种操作？ 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入
+//["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+//[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+//输出
+//[null, null, null, 1, null, -1, null, -1, 3, 4]
+//
+//解释
+//LRUCache lRUCache = new LRUCache(2);
+//lRUCache.put(1, 1); // 缓存是 {1=1}
+//lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+//lRUCache.get(1);    // 返回 1
+//lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+//lRUCache.get(2);    // 返回 -1 (未找到)
+//lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+//lRUCache.get(1);    // 返回 -1 (未找到)
+//lRUCache.get(3);    // 返回 3
+//lRUCache.get(4);    // 返回 4
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= capacity <= 3000 
+// 0 <= key <= 10000 
+// 0 <= value <= 105 
+// 最多调用 2 * 105 次 get 和 put 
+// 
+// Related Topics 设计 哈希表 链表 双向链表 
+// 👍 1585 👎 0
+
+
+package com.mengfg.leetcode.editor.cn;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Problem_146_LruCache {
+    public static void main(String[] args) {
+        LRUCache lRUCache = new Problem_146_LruCache.LRUCache(2);
+        lRUCache.put(1, 1); // 缓存是 {1=1}
+        lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+        System.out.println(lRUCache.get(1));
+        lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+        System.out.println(lRUCache.get(2));
+        lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+        System.out.println(lRUCache.get(1));
+        System.out.println(lRUCache.get(3));
+        System.out.println(lRUCache.get(4));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    static class LRUCache {
+        int size;
+        int capacity;
+        Node head;
+        Node tail;
+        Map<Integer, Node> map;
+
+        public LRUCache(int capacity) {
+            this.capacity = capacity;
+            size = 0;
+            map = new HashMap<>(capacity);
+            head = new Node();
+            tail = new Node();
+            head.next = tail;
+            tail.prev = head;
+        }
+
+        public int get(int key) {
+            if (!map.containsKey(key)) {
+                return -1;
+            }
+
+            Node newHead = map.get(key);
+            moveToHead(newHead);
+            return newHead.value;
+        }
+
+        public void put(int key, int value) {
+
+            if (size == 0) {
+                Node node = new Node(key, value);
+                addToHead(node);
+                map.put(key, node);
+                size++;
+                return;
+            }
+
+            // 包含key，修改值并添加到队列首部
+            if (map.containsKey(key)) {
+                map.get(key).value = value;
+                moveToHead(map.get(key));
+                return;
+            }
+
+            // 不包含key，添加中到首部，size++
+            Node node = new Node(key, value);
+            addToHead(node);
+            map.put(key, node);
+            size++;
+
+            // 队列已满，删除最后一个
+            if (size > capacity) {
+                map.remove(tail.prev.key);
+                removeNode(tail.prev);
+                size--;
+            }
+        }
+
+        private void addToHead(Node node) {
+            Node next = head.next;
+            next.prev = node;
+            node.next = next;
+            node.prev = head;
+            head.next = node;
+        }
+
+        private void moveToHead(Node node) {
+            removeNode(node);
+            addToHead(node);
+        }
+
+        private void removeNode(Node node) {
+            Node prev = node.prev;
+            Node next = node.next;
+            prev.next = next;
+            next.prev = prev;
+        }
+
+    }
+
+    static class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+
+        public Node() {
+        }
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
